ee5b8fe25ac98486daeb58a4c8183301cc5fea21
Szabolcs Tóth <tsz@purzelbaum.hu>
2026-04-10 11:23:00 +0200

    Local and remote database connection tests were added.

--- Tests/SwiftCypherTests/SwiftCypherTests.swift
+++ Tests/SwiftCypherTests/SwiftCypherTests.swift
@@ -29,20 +29,24 @@ struct SwiftCypherTests {
     ])
   }
 
-  // MARK: -
-  @Test("Test if client can conect to local database.")
+  // MARK: - Local database test
+  @Test("Client connects to local database, makes a query and bookmark is not empty.")
   func localDBConnectionTest() async throws {
     guard let username = reader.string(forKey: "USERNAME"),
       let password = reader.string(forKey: "PASSWORD")
     else {
       throw SwiftCypherError.missingCredentials
     }
+
     let client = SwiftCypherClient(username: username, password: password)
-    try await client.runQuery()
+    let request = QueryRequest(statement: "MATCH (n:FRIEND) RETURN n.name")
+
+    let response = try await client.runQuery(request: request)
+    #expect(!response.bookmarks.isEmpty)
   }
 
-  // MARK: -
-  @Test("Test if client can conect to remote database.")
+  // MARK: - Aurora/Remote database connection test
+  @Test("Client connects to Aurora/remote database, makes a query and bookmark is not empty.")
   func remoteDBConnectionTest() async throws {
     guard let db = reader.string(forKey: "NEO4J_DATABASE") else {
       throw SwiftCypherError.missingDatabaseName(key: "NEO4J_DATABASE")
@@ -54,6 +58,10 @@ struct SwiftCypherTests {
     }
 
     let client = SwiftCypherClient(service: .aura(database: db), username: username, password: password)
-    try await client.runQuery()
+      // CREATE (alice:FRIEND {name: 'Alice'})
+      let request = QueryRequest(statement: "CREATE (szabolcs:FRIEND {name: $name})", parameters: ["name": .string("Szabolcs")])
+
+    let response = try await client.runQuery(request: request)
+    #expect(!response.bookmarks.isEmpty)
   }
 }
