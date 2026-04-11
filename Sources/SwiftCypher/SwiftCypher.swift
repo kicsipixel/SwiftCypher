cc9d03a915b97d82cbd61806639145ba9ff9d888
Szabolcs Tóth <tsz@purzelbaum.hu>
2026-04-12 01:22:22 +0200

    JSON coding error fixed.

--- Sources/SwiftCypher/SwiftCypher.swift
+++ Sources/SwiftCypher/SwiftCypher.swift
@@ -49,7 +49,7 @@ public struct SwiftCypherClient: Sendable {
 
     var urlRequest = URLRequest(url: url)
     urlRequest.httpMethod = "POST"
-    urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    urlRequest.setValue("application/vnd.neo4j.query.v1.1", forHTTPHeaderField: "Content-Type")
     urlRequest.setValue("application/vnd.neo4j.query.v1.1", forHTTPHeaderField: "Accept")
     let credentials = "\(username):\(password)".data(using: .utf8)!.base64EncodedString()
     urlRequest.addValue("Basic \(credentials)", forHTTPHeaderField: "Authorization")
